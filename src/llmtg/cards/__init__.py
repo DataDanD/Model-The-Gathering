862f8d8fac652634c844af773977f2dee205d693
Daniel Meurer <29312246+DataDanD@users.noreply.github.com>
2026-09-16 22:27:11 -0600

    Export default bulk sync helper

--- src/llmtg/cards/__init__.py
+++ src/llmtg/cards/__init__.py
@@ -1,4 +1,9 @@
-from llmtg.cards.bulk import BulkDataError, BulkScryfallProvider, ensure_oracle_bulk_data
+from llmtg.cards.bulk import (
+    BulkDataError,
+    BulkScryfallProvider,
+    ensure_default_bulk_data,
+    ensure_oracle_bulk_data,
+)
 from llmtg.cards.models import Card
 from llmtg.cards.provider import CardNotFoundError, CardProvider
 from llmtg.cards.scryfall import ScryfallProvider
@@ -10,5 +15,6 @@ __all__ = [
     "CardNotFoundError",
     "CardProvider",
     "ScryfallProvider",
+    "ensure_default_bulk_data",
     "ensure_oracle_bulk_data",
 ]
